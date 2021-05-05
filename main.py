7b6f4c24ecb5c05ae469c912a0e84e5a54c77acb
Ashish Porecha <ashishporecha@Ashishs-MacBook-Air.local>
2021-05-05 16:17:27 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+def addFunc(number:int, number2:int):
+    return number+number2
+
+def subtractFunc(number:int, number2:int):
+    return number-number2
+
+if __name__ == "__main__": 
+    print(addFunc(3, 5))
+    print(subtractFunc(5, 2))
+
+
